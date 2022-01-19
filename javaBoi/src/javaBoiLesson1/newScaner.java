1ab4470bdb335226396cd12202f81befa58f76b8
HerliKhoury <herli.kh@gmail.com>
2022-01-19 20:46:13 -0300

    Limpa o buffer de leitura

--- javaBoi/src/javaBoiLesson1/newScaner.java
+++ javaBoi/src/javaBoiLesson1/newScaner.java
@@ -8,13 +8,17 @@ public class newScaner {
 		
 		Scanner sc = new Scanner(System.in);
 
+		int x;
 		String s1, s2, s3;
-
+		
+		x = sc.nextInt();
+		sc.nextLine();//Elimina espaçõ criado entre informações
 		s1 = sc.nextLine();// Armazena tudo que foi escrito antes de apertar "Enter"
 		s2 = sc.nextLine();
 		s3 = sc.nextLine();
 
 		System.out.println("DADOS DIGITADOS:");
+		System.out.println(x);
 		System.out.println(s1);
 		System.out.println(s2);
 		System.out.println(s3);
