6906f362fb4ce826ebef4274c63dadbc2ceda348
HerliKhoury <herli.kh@gmail.com>
2022-01-24 20:23:59 -0300

    ler dois valores inteiros, e depois mostrar na tela a soma desses números

---
+++ javaBoi/src/javaBoiLesson1/Exercício_2_1.java
@@ -0,0 +1,25 @@
+package javaBoiLesson1;
+
+import java.util.Scanner;
+
+
+public class Exercício_2_1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int x, y;
+		
+		
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		
+		System.out.printf("Soma = %d", x + y);
+		
+		sc.close();
+
+	}
+
+}
